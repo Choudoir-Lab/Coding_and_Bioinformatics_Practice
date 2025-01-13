25d3dc5737cc8d165906552ef9fde0154717c117
ambreist <ambreist@ncsu.edu>
2025-01-13 11:47:16 -0500

    Created python script to parse raw output of Biolog EcoPlate absorbance readings from Tecan i-control software.

---
+++ Python_Scripts/Functional_Potential_Workflow/Metabolic_Analyses/ecoplate_calculations.py
@@ -0,0 +1,232 @@
+import pandas as pd
+import sys
+import os
+import argparse
+import textwrap
+
+parser = argparse.ArgumentParser(prog="ecoplate_calculations.py", 
+                                 formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description=textwrap.dedent('''\
+                                                             Tool to organize output data of Biolog EcoPlate readings from the Tecan i-control software.
+                                                             -------------------------------------------------------------------------------------------
+                                                             This tool currently only takes readings of one wavelength per sample.
+
+                                                             The path specified must include all excel spreadsheet outputs from the Tecan i-control software.
+                                                             
+                                                             The sep argument is there for grouping results based on metadata. For example, if there are two 
+                                                             sample locations, the argument would be 'Location1,Location2'. For this to work properly, this
+                                                             text string must be within the filename of each output spreadsheet for that group (case-sensitive).
+                                                             If there is only one grouping, still include the name of the grouping but with no comma.
+                                                             
+                                                             For the line argument, specify the row number of the header from the output readings in the raw
+                                                             data document. For the default excel output from the i-control software, the first column of the
+                                                             raw reads will be denoted with '<>'.
+                                                             
+                                                             The output files will appear in the same directory as this script is run from'''),
+                                 epilog="Written by Adam Breister, ambreist@ncsu.edu")
+
+parser.add_argument('-p', '--path', help="Specify path to directory with plate reader output excel spreadsheets (default: './')", nargs=1, default="./")
+parser.add_argument('-s', '--sep', help="Provide comma-separated list of names that group the different spreadsheets", nargs=1, default=None, required=True)
+parser.add_argument('-l', '--line', help="Specify the row number that the header for the raw data table is (default: '30')", nargs=1, default=30, type=int)
+
+args = parser.parse_args()
+
+path_to_files = args.path[0]
+separator = args.sep[0]
+start_line = args.line[0]
+
+
+def read_input_spreadsheets(input_file,start):
+    rows_to_skip = start - 1
+    input_spreadsheet = pd.read_excel(input_file, skiprows=rows_to_skip, nrows=9, usecols="B:M", sheet_name=None)
+
+    ecoplate_value_dict = {}
+
+    for sample_names in input_spreadsheet.keys():
+        list_of_rows_spreadsheet = input_spreadsheet[sample_names].values.tolist()
+
+        flat_list = []
+
+        for lists in list_of_rows_spreadsheet:
+            for item in lists:
+                flat_list.append(item)
+
+        ecoplate_value_dict[sample_names] = flat_list
+
+    return ecoplate_value_dict
+
+
+directory_list = os.listdir(path_to_files)
+full_reordered_data_dict = {}
+
+print("Files Being Read:\n")
+for filename in directory_list:
+    if filename.endswith(".xlsx") and "~" not in filename:
+        relative_path = path_to_files + filename
+        print(relative_path)
+        full_reordered_data_dict[filename.rsplit(".", 1)[0] + "_reordered"] = read_input_spreadsheets(relative_path, start_line)
+print("\nDone Reading Files")
+
+well_ids = []
+
+for i in range(65,73):
+    for n in range(1,13):
+        well_ids.append(chr(i) + str(n))
+
+list_of_substrates = ["Water","β-Methyl-D-Glucoside","D-Galactonic Acid γ-Lactone",
+"L-Arginine",
+"Water",
+"β-Methyl-D-Glucoside",
+"D-Galactonic Acid γ-Lactone",
+"L-Arginine",
+"Water",
+"β-Methyl-D-Glucoside",
+"D-Galactonic Acid γ-Lactone",
+"L-Arginine",
+"Pyruvic Acid Methyl Ester",
+"D-Xylose",
+"D-Galacturonic Acid",
+"L-Asparagine",
+"Pyruvic Acid Methyl Ester",
+"D-Xylose",
+"D-Galacturonic Acid",
+"L-Asparagine",
+"Pyruvic Acid Methyl Ester",
+"D-Xylose",
+"D-Galacturonic Acid",
+"L-Asparagine",
+"Tween 40",
+"i-Erythritol",
+"2-Hydroxy Benzoic Acid",
+"L-Phenylalanine",
+"Tween 40",
+"i-Erythritol",
+"2-Hydroxy Benzoic Acid",
+"L-Phenylalanine",
+"Tween 40",
+"i-Erythritol",
+"2-Hydroxy Benzoic Acid",
+"L-Phenylalanine",
+"Tween 80",
+"D-Mannitol",
+"4-Hydroxy Benzoic Acid",
+"L-Serine",
+"Tween 80",
+"D-Mannitol",
+"4-Hydroxy Benzoic Acid",
+"L-Serine",
+"Tween 80",
+"D-Mannitol",
+"4-Hydroxy Benzoic Acid",
+"L-Serine",
+"α-Cyclodextrin",
+"N-Acetyl-D-Glucosamine",
+"γ-Amino Butyric Acid ",
+"L-Threonine",
+"α-Cyclodextrin",
+"N-Acetyl-D-Glucosamine",
+"γ-Amino Butyric Acid ",
+"L-Threonine",
+"α-Cyclodextrin",
+"N-Acetyl-D-Glucosamine",
+"γ-Amino Butyric Acid ",
+"L-Threonine",
+"Glycogen",
+"D-Glucosaminic Acid",
+"Itaconic Acid",
+"β-HydroxyGlycyl-L-Glutamic Acid",
+"Glycogen",
+"D-Glucosaminic Acid",
+"Itaconic Acid",
+"β-HydroxyGlycyl-L-Glutamic Acid",
+"Glycogen",
+"D-Glucosaminic Acid",
+"Itaconic Acid",
+"β-HydroxyGlycyl-L-Glutamic Acid",
+"D-Cellobiose",
+"Glucose1-Phosphate",
+"α-Keto Butyric Acid",
+"Phenylethylamine",
+"D-Cellobiose",
+"Glucose1-Phosphate",
+"α-Keto Butyric Acid",
+"Phenylethylamine",
+"D-Cellobiose",
+"Glucose1-Phosphate",
+"α-Keto Butyric Acid",
+"Phenylethylamine",
+"α-D-Lactose",
+"D,L-α-Glycerol Phosphate",
+"D-Malic Acid",
+"Putrescine",
+"α-D-Lactose",
+"D,L-α-Glycerol Phosphate",
+"D-Malic Acid",
+"Putrescine",
+"α-D-Lactose",
+"D,L-α-Glycerol Phosphate",
+"D-Malic Acid",
+"Putrescine"]
+
+well_metadata_df = pd.DataFrame({"Well Name":well_ids, "Well Substrate":list_of_substrates})
+
+with pd.ExcelWriter("Reordered_Ecoplate_Data.xlsx") as writer:
+    for sheetname in full_reordered_data_dict.keys():
+        values_dataframe = pd.DataFrame(full_reordered_data_dict[sheetname])
+        metadata_values_dataframe = pd.concat([well_metadata_df, values_dataframe], axis=1)
+
+        metadata_values_dataframe.to_excel(writer, sheet_name=sheetname, index=False)
+
+
+def calculate_awcd(all_data_dict, metadata):
+    combined_awcd = {}
+    
+    for sheetname_2 in all_data_dict.keys():
+        only_values_dataframe = pd.DataFrame(all_data_dict[sheetname_2])
+        ecoplate_data = pd.concat([metadata, only_values_dataframe], axis=1)
+
+        ecoplate_data.drop("Well Name", axis=1, inplace=True)
+
+
+        grouped_average_ecoplate_data = ecoplate_data.groupby("Well Substrate").mean()
+        grouped_average_ecoplate_data.reset_index(inplace=True)
+
+
+        water_index = grouped_average_ecoplate_data.loc[grouped_average_ecoplate_data["Well Substrate"]=="Water", :].index[0]
+        water_absorbance_series = grouped_average_ecoplate_data.iloc[water_index]
+        grouped_average_ecoplate_data.drop([water_index], inplace=True)
+        new_water_absorbance_series = water_absorbance_series.drop(labels='Well Substrate')
+
+        subtracted_all_absorbance = grouped_average_ecoplate_data.subtract(new_water_absorbance_series).drop(["Well Substrate"], axis=1)
+        subtracted_all_absorbance[subtracted_all_absorbance < 0] = 0
+
+        average_well_color_development = subtracted_all_absorbance.sum()/31
+        average_well_color_development.name = sheetname_2.rsplit("_", 1)[0]
+
+        combined_awcd[sheetname_2] = average_well_color_development
+
+    return combined_awcd
+
+awcd_dict = calculate_awcd(full_reordered_data_dict, well_metadata_df)
+
+
+with pd.ExcelWriter("Ecoplate_Average_Well_Color_Development.xlsx") as writer_2:
+    if "," in separator:
+        for splits in separator.split(","):
+            specific_series = pd.Series()
+            for names in awcd_dict.keys():
+                if splits in names:
+                    specific_series = pd.concat([specific_series, awcd_dict[names]], axis=1)
+            
+            specific_series.drop(0, axis=1, inplace=True)
+
+            specific_series.to_excel(writer_2, sheet_name=splits, index=True)
+    else:
+        specific_series = pd.Series()
+        for names in awcd_dict.keys():
+            if separator in names:
+                specific_series = pd.concat([specific_series, awcd_dict[names]], axis=1)
+            
+        specific_series.drop(0, axis=1, inplace=True)
+
+        specific_series.to_excel(writer_2, sheet_name=separator, index=True)
