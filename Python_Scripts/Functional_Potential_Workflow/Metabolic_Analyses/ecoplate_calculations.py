315507c58f6f8a0ff636f341f7edd22ae1198564
ambreist <ambreist@ncsu.edu>
2025-01-16 14:47:26 -0500

    Re-worded description and added argument to specify output directory.

--- Python_Scripts/Functional_Potential_Workflow/Metabolic_Analyses/ecoplate_calculations.py
+++ Python_Scripts/Functional_Potential_Workflow/Metabolic_Analyses/ecoplate_calculations.py
@@ -13,28 +13,38 @@ parser = argparse.ArgumentParser(prog="ecoplate_calculations.py",
 
                                                              The path specified must include all excel spreadsheet outputs from the Tecan i-control software.
                                                              
-                                                             The sep argument is there for grouping results based on metadata. For example, if there are two 
-                                                             sample locations, the argument would be 'Location1,Location2'. For this to work properly, this
-                                                             text string must be within the filename of each output spreadsheet for that group (case-sensitive).
-                                                             If there is only one grouping, still include the name of the grouping but with no comma.
+                                                             The sep argument is there for grouping results based on groups of excel output spreadsheets. For example, 
+                                                             if there are two sample locations separated in different spreadsheets (ex. Location1_Day#.xlsx, Location2_Day#.xslx), 
+                                                             the user would input 'Location1,Location2' for this argument. The text strings used must be present within the
+                                                             output spreadsheets (it is also case-sensitive). If there is only one grouping, still include the name of the grouping
+                                                             but without a comma.
                                                              
                                                              For the line argument, specify the row number of the header from the output readings in the raw
                                                              data document. For the default excel output from the i-control software, the first column of the
                                                              raw reads will be denoted with '<>'.
                                                              
-                                                             The output files will appear in the same directory as this script is run from'''),
+                                                             The out argument specifies the directory where the output files will created'''),
                                  epilog="Written by Adam Breister, ambreist@ncsu.edu")
 
 parser.add_argument('-p', '--path', help="Specify path to directory with plate reader output excel spreadsheets (default: './')", nargs=1, default="./")
-parser.add_argument('-s', '--sep', help="Provide comma-separated list of names that group the different spreadsheets", nargs=1, default=None, required=True)
+parser.add_argument('-s', '--substrate', help="Specify path to text file with ordered list of substrates (A1,A2...B1,B2...H11,H12) with each substrate on separate line", nargs=1, required=True)
 parser.add_argument('-l', '--line', help="Specify the row number that the header for the raw data table is (default: '30')", nargs=1, default=30, type=int)
+parser.add_argument('-o', '--out', help="Specify directory where output files will be generated (default: './')", nargs=1, default="./", type=str)
 
 args = parser.parse_args()
 
-path_to_files = args.path[0]
-separator = args.sep[0]
+substrates = args.substrate[0]
 start_line = args.line[0]
 
+if args.path[0].endswith("/"):
+    path_to_files = args.path[0]
+else:
+    path_to_files = args.path[0] + "/"
+
+if args.out[0].endswith("/"):
+    output_directory = args.out[0]
+else:
+    output_directory = args.out[0] + "/"
 
 def read_input_spreadsheets(input_file,start):
     rows_to_skip = start - 1
@@ -43,15 +53,18 @@ def read_input_spreadsheets(input_file,start):
     ecoplate_value_dict = {}
 
     for sample_names in input_spreadsheet.keys():
-        list_of_rows_spreadsheet = input_spreadsheet[sample_names].values.tolist()
+        if sample_names == "Sheet1":
+            continue
+        else:
+            list_of_rows_spreadsheet = input_spreadsheet[sample_names].values.tolist()
 
-        flat_list = []
+            flat_list = []
 
-        for lists in list_of_rows_spreadsheet:
-            for item in lists:
-                flat_list.append(item)
+            for lists in list_of_rows_spreadsheet:
+                for item in lists:
+                    flat_list.append(item)
 
-        ecoplate_value_dict[sample_names] = flat_list
+            ecoplate_value_dict[sample_names] = flat_list
 
     return ecoplate_value_dict
 
@@ -170,7 +183,7 @@ list_of_substrates = ["Water","β-Methyl-D-Glucoside","D-Galactonic Acid γ-Lact
 
 well_metadata_df = pd.DataFrame({"Well Name":well_ids, "Well Substrate":list_of_substrates})
 
-with pd.ExcelWriter("Reordered_Ecoplate_Data.xlsx") as writer:
+with pd.ExcelWriter(output_directory + "Reordered_Ecoplate_Data.xlsx") as writer:
     for sheetname in full_reordered_data_dict.keys():
         values_dataframe = pd.DataFrame(full_reordered_data_dict[sheetname])
         metadata_values_dataframe = pd.concat([well_metadata_df, values_dataframe], axis=1)
@@ -210,7 +223,7 @@ def calculate_awcd(all_data_dict, metadata):
 awcd_dict = calculate_awcd(full_reordered_data_dict, well_metadata_df)
 
 
-with pd.ExcelWriter("Ecoplate_Average_Well_Color_Development.xlsx") as writer_2:
+with pd.ExcelWriter(output_directory + "Ecoplate_Average_Well_Color_Development.xlsx") as writer_2:
     if "," in separator:
         for splits in separator.split(","):
             specific_series = pd.Series()
